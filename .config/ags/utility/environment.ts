06d8c85db072cf501895d3425fc3f506f325fa19
R3dKar <aazz.140720045@gmail.com>
2025-01-10 07:38:00 +0300

    feat: add full .env support

--- .config/ags/utility/environment.ts
+++ .config/ags/utility/environment.ts
@@ -1,7 +1,22 @@
-import env from 'inline:../.env';
-import envExample from 'inline:../.env.example';
+import envFile from 'inline:../.env';
+import envFileExample from 'inline:../.env.example';
 
-console.log(env);
-console.log(envExample);
+const lineRegex = /^(?<param>\w+)\s*=\s*(?<quote>['"]?)(?<value>.*)\k<quote>\s*/gm;
 
-// TODO: implement it!!!
+let data: Record<string, string> = {};
+
+const envLineMatches = envFile.split('\n').map(line => line.trim()).join('\n').matchAll(lineRegex)
+
+for (const line of envLineMatches) {
+  const [, parameter, , value] = line;
+  data[parameter] = value;
+}
+
+const envLineExampleMatches = envFileExample.split('\n').map(line => line.trim()).join('\n').matchAll(lineRegex);
+
+for (const line of envLineExampleMatches) {
+  const [, parameter] = line;
+  if (!(parameter in data)) throw new Error(`Parameter "${parameter}" is not set`);
+}
+
+export const env = data;
--- .config/ags/utility/weather.ts
+++ .config/ags/utility/weather.ts
@@ -2,6 +2,7 @@ import { Variable } from 'astal';
 import { ServiceData, ServiceStatus } from './service';
 import { location } from './location';
 import { fetchJsonAsync } from './network';
+import { env } from './environment';
 
 export interface Weather {
   temperature: {
@@ -16,8 +17,6 @@ export type WeatherData = ServiceData<Weather>;
 
 export const weather = Variable<WeatherData>({ status: ServiceStatus.Unavailable });
 
-const API_KEY = '';
-
 const updateWeather = async (): Promise<WeatherData> => {
   const previousLocation = location.get();
   const previousWeather = weather.get();
@@ -27,7 +26,7 @@ const updateWeather = async (): Promise<WeatherData> => {
   const { latitude, longitude } = previousLocation;
 
   try {
-    const data = await fetchJsonAsync(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=ru&appid=${API_KEY}`, 10*1000, 10);
+    const data = await fetchJsonAsync(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=ru&appid=${env.WEATHER_API_KEY}`, 10*1000, 10);
     return {
       status: ServiceStatus.Available,
       temperature: {
