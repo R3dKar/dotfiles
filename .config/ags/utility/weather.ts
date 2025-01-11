df42e5821513096eb9425c6771d44cdd5972f2d9
R3dKar <aazz.140720045@gmail.com>
2025-01-11 10:26:52 +0300

    feat: add full feature weather info

--- .config/ags/utility/weather.ts
+++ .config/ags/utility/weather.ts
@@ -4,13 +4,21 @@ import { location } from './location';
 import { fetchJsonAsync } from './network';
 import { env } from './environment';
 
+export enum Precipitation {
+  None,
+  Rain,
+  Thunderstorm,
+  Snow,
+}
+
 export interface Weather {
   temperature: {
-    now: number,
-    min: number,
-    max: number,
+    real: number,
     feel: number
-  }
+  },
+  precipitation: Precipitation,
+  clouds: number,
+  death: boolean
 };
 
 export type WeatherData = ServiceData<Weather>;
@@ -23,19 +31,40 @@ const updateWeather = async (): Promise<WeatherData> => {
 
   if (previousLocation.status === ServiceStatus.Unavailable) return previousWeather;
 
-  const { latitude, longitude } = previousLocation;
+  let { latitude, longitude } = previousLocation;
 
   try {
     const data = await fetchJsonAsync(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=ru&appid=${env.WEATHER_API_KEY}`, 10*1000, 10);
 
+    console.log(data);
+
+    const temperature = {
+      real: data.main.temp as number,
+      feel: data.main.feels_like as number
+    };
+
+    const statuses: string[] = data.weather.map((item: any) => item.id.toFixed());
+
+    let precipitation = Precipitation.None;
+
+    if (statuses.some(status => status.startsWith('2')))
+      precipitation = Precipitation.Thunderstorm;
+    else if (statuses.some(status => status.startsWith('6')))
+      precipitation = Precipitation.Snow;
+    else if (statuses.some(status => status.startsWith('3') || status.startsWith('5')))
+      precipitation = Precipitation.Rain;
+
+    let death =
+      temperature.feel < -25 || temperature.feel > 28 ||
+      statuses.some(status => status in ['781', '762', '622', '602', '522', '504', '503', '502', '314', '312', '302', '232', '212', '202'])
+    ;
+
     return {
       status: ServiceStatus.Available,
-      temperature: {
-        now: data.main.temp,
-        min: data.main.temp_min,
-        max: data.main.temp_max,
-        feel: data.main.feels_like
-      }
+      temperature,
+      precipitation,
+      clouds: data.clouds.all,
+      death
     };
   } catch {
     return previousWeather;
--- .config/ags/widgets/topbar/elements/WeatherInfo.tsx
+++ .config/ags/widgets/topbar/elements/WeatherInfo.tsx
@@ -1,17 +1,67 @@
 import { ServiceStatus } from '@/utility/service';
-import { weather } from '@/utility/weather';
+import { Precipitation, weather } from '@/utility/weather';
+import { Variable } from 'astal';
+import { Gdk } from 'astal/gtk4';
+
+enum WeatherTemperatureState {
+  Real,
+  Feel
+};
 
 export default () => {
-  const weatherLabelBind = weather(weather => {
-    if (weather.status === ServiceStatus.Unavailable) return '???';
+  const availableBind = weather(({ status }) => status === ServiceStatus.Available);
 
-    return `${weather.temperature.feel.toFixed()}°C`;
+  const weatherIconBind = weather(weather => {
+    if (weather.status === ServiceStatus.Unavailable) return '';
+
+    if (weather.death) return ' ';
+
+    const precipitationIconMap = {
+      [Precipitation.Rain]: '',
+      [Precipitation.Thunderstorm]: '󱐋',
+      [Precipitation.Snow]: ' '
+    };
+    if (weather.precipitation !== Precipitation.None) return precipitationIconMap[weather.precipitation];
+
+    if (weather.clouds > 25) return '󰖐 ';
+
+    return ' ';
   });
 
+  const temperatureState = Variable(WeatherTemperatureState.Real);
+
+  const temperatureStateLabelMap = {
+    [WeatherTemperatureState.Real]: <label>
+      {weather(weather => {
+        if (weather.status === ServiceStatus.Unavailable) return '';
+
+        return `${weather.temperature.real.toFixed()}°C`;
+      })}
+    </label>,
+    [WeatherTemperatureState.Feel]: <label>
+      {weather(weather => {
+        if (weather.status === ServiceStatus.Unavailable) return '';
+
+        return `󰾞${weather.temperature.feel.toFixed()}°C`;
+      })}
+    </label>
+  };
+
+  const onClick = (_: any, event: Gdk.ButtonEvent): void => {
+    if (event.get_button() !== Gdk.BUTTON_PRIMARY) return;
+
+    if (temperatureState.get() === WeatherTemperatureState.Real) temperatureState.set(WeatherTemperatureState.Feel)
+    else temperatureState.set(WeatherTemperatureState.Real);
+  };
+
+
   return (
-    <box spacing={3}>
-      <label>󰖐 </label>
-      <label>{weatherLabelBind}</label>
+    <box
+      onButtonReleased={onClick}
+      visible={availableBind}
+    >
+      <label>{weatherIconBind}</label>
+      {temperatureState(state => temperatureStateLabelMap[state])}
     </box>
   );
 };
