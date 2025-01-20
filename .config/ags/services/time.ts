1b0587c18df4e838881e6d7ab5ee4653a665d442
R3dKar <aazz.140720045@gmail.com>
2025-01-20 21:00:57 +0300

    feat: change time to use GLib methods not date

--- .config/ags/services/time.ts
+++ .config/ags/services/time.ts
@@ -1,7 +1,8 @@
 import { Variable } from 'astal';
+import GLib from 'gi://GLib';
 
 const DATE_FORMAT = '%T %a %d.%m.%Y';
 const POLLING_INTERVAL = 200;
 
 export const time = Variable('');
-time.poll(POLLING_INTERVAL, `date +"${DATE_FORMAT}"`);
+time.poll(POLLING_INTERVAL, () => GLib.DateTime.new_now_local().format(DATE_FORMAT)!);
