650514c178a81cbcc1351cf5b9952ade6b16512a
R3dKar <aazz.140720045@gmail.com>
2025-02-14 04:35:51 +0300

    chore: update dependencies

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Dependencies
-yay -S hyprland uwsm noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-nerd-fonts-symbols \
-ttf-nerd-fonts-symbols-mono ttf-meslo-nerd zsh-theme-powerlevel10k-git \
-alacritty-sixel-git libsixel imagemagick btop stow nvidia-dkms libva-nvidia-driver \
-lib32-nvidia-utils  pipewire python-requests python-dotenv python-aioconsole \
+yay -S --needed hyprland uwsm noto-fonts noto-fonts-emoji noto-fonts-cjk
+ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-jetbrains-mono ttf-jetbrains-mono-nerd \
+kitty zsh-theme-powerlevel10k-git btop stow nvidia-dkms libva-nvidia-driver \
+lib32-nvidia-utils pipewire python-requests python-dotenv python-aioconsole \
 pacman-contrib pipewire-audio pipewire-pulse wireplumber xdg-desktop-portal-hyprland \
 hyprpolkitagent qt5-wayland qt6-wayland socat hyprpaper hyprpicker slurp grim \
 cliphist playerctl xdg-user-dirs arc-icon-theme arc-solid-gtk-theme nwg-look \
